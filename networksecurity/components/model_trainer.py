3a9de044e12aefbc282ab87e42b256c0c8c20bdf
bhanu1836 <itzbhanu00@gmail.com>
2025-03-28 18:52:18 +0530

    model training setuped

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -112,6 +112,6 @@ class ModelTrainer:
                 test_arr[:,-1]
             )
 
-            model = self.train_model(x_train,y_train)
+            model = self.train_model(x_train,y_train,x_test,y_test)
         except Exception as e:
             raise NetworkSecurityException(e,sys)
