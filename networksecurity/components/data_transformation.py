51c9e6d4b49796fed15b255b23cdb4513cb23f51
bhanu1836 <itzbhanu00@gmail.com>
2025-03-28 10:36:30 +0530

    Data Tranformation setup

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -1,15 +1,115 @@
-import os,sys
+import sys
+import os
 import numpy as np
 import pandas as pd
 from sklearn.impute import KNNImputer
 from sklearn.pipeline import Pipeline
-from networksecurity.constant import training_pipeline
+from sklearn.preprocessing import OrdinalEncoder  # Changed from LabelEncoder
+from sklearn.compose import ColumnTransformer
+
 from networksecurity.constant.training_pipeline import TARGET_COLUMN
-from networksecurity.constant.training_pipeline import DATA_TRANSFORMATION_INPUTER_PARAMS
-from networksecurity.entity.artifact_entity import DataTransformationArtifact,DataValidationArtifact
+from networksecurity.constant.training_pipeline import DATA_TRANSFORMATION_IMPUTER_PARAMS
+
+from networksecurity.entity.artifact_entity import (
+    DataTransformationArtifact,
+    DataValidationArtifact
+)
 
 from networksecurity.entity.config_entity import DataTransformationConfig
-from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.exception.exception import NetworkSecurityException 
 from networksecurity.logging.logger import logging
-
 from networksecurity.utils.main_utils.utils import save_numpy_array_data,save_object
+
+class DataTransformation:
+    def __init__(self,data_validation_artifact:DataValidationArtifact,
+                 data_transformation_config:DataTransformationConfig):
+        try:
+            self.data_validation_artifact:DataValidationArtifact=data_validation_artifact
+            self.data_transformation_config:DataTransformationConfig=data_transformation_config
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
+        
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def get_data_transformer_object(self, input_feature_train_df: pd.DataFrame) -> Pipeline:
+        try:
+            logging.info("Creating data transformer object")
+            
+            # Identify categorical and numerical columns
+            categorical_columns = input_feature_train_df.select_dtypes(include=['object']).columns
+            numerical_columns = input_feature_train_df.select_dtypes(exclude=['object']).columns
+            
+            # Create preprocessing pipelines
+            categorical_pipeline = Pipeline([
+                ('ordinal_encoder', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1))  # Changed from LabelEncoder
+            ])
+            
+            numerical_pipeline = Pipeline([
+                ('imputer', KNNImputer(**DATA_TRANSFORMATION_IMPUTER_PARAMS))
+            ])
+            
+            preprocessor = ColumnTransformer(
+                transformers=[
+                    ('num', numerical_pipeline, numerical_columns),
+                    ('cat', categorical_pipeline, categorical_columns)
+                ],
+                remainder='passthrough'  # Added to handle any columns not explicitly specified
+            )
+            
+            return preprocessor
+            
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+        
+    def initiate_data_transformation(self) -> DataTransformationArtifact:
+        try:
+            logging.info("Starting data transformation")
+            train_df = DataTransformation.read_data(self.data_validation_artifact.valid_train_file_path)
+            test_df = DataTransformation.read_data(self.data_validation_artifact.valid_test_file_path)
+
+            # Training dataframe
+            input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN], axis=1)
+            target_feature_train_df = train_df[TARGET_COLUMN]
+            target_feature_train_df = target_feature_train_df.replace(-1, 0)
+
+            # Testing dataframe
+            input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN], axis=1)
+            target_feature_test_df = test_df[TARGET_COLUMN]
+            target_feature_test_df = target_feature_test_df.replace(-1, 0)
+
+            preprocessor = self.get_data_transformer_object(input_feature_train_df)
+            
+            # Fit and transform the data
+            transformed_input_train_feature = preprocessor.fit_transform(input_feature_train_df)
+            transformed_input_test_feature = preprocessor.transform(input_feature_test_df)
+
+            train_arr = np.c_[transformed_input_train_feature, np.array(target_feature_train_df) ]
+            test_arr = np.c_[ transformed_input_test_feature, np.array(target_feature_test_df) ]
+
+            #save numpy array data
+            save_numpy_array_data( self.data_transformation_config.transformed_train_file_path, array=train_arr, )
+            save_numpy_array_data( self.data_transformation_config.transformed_test_file_path,array=test_arr,)
+            save_object( self.data_transformation_config.transformed_object_file_path, preprocessor,)
+
+            save_object( "final_model/preprocessor.pkl", preprocessor,)
+
+
+            #preparing artifacts
+
+            data_transformation_artifact=DataTransformationArtifact(
+                transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
+                transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                transformed_test_file_path=self.data_transformation_config.transformed_test_file_path
+            )
+            return data_transformation_artifact
+
+
+            
+        except Exception as e:
+            raise NetworkSecurityException(e,sys)
--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -39,9 +39,9 @@ DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
 DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
 DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
 
-DATA_TRANSFORMATION_INPUTER_PARAMS: dict = {
+DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
     "missing_values": np.nan,
-    "n_neighbours": 3,
+    "n_neighbors": 3,
     "weights": "uniform"
 }
 
